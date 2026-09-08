55bb00bffafcd28df86d19403983037be3abf83e
mrfahad65 <mrfahad656788@gmail.com>
2026-09-08 15:13:44 +0500

    Add MUNEEBXFILES2 entry to keylist.lua

--- keylist.lua
+++ keylist.lua
@@ -1,4 +1,4 @@
--- keylist.lua - GitHub pe rakho
+Yu-- keylist.lua - GitHub pe rakho
 -- max_devices = 1 (matlab sirf ek phone)
 -- max_devices = 20 (matlab 20 phones mein chalegi)
 
@@ -43,6 +43,13 @@ return {
         expiry = "2026-09-20",
         valid = true,
         max_devices = 100,
+        SLOT = "2"
+    },
+      ["MUNEEBXFILES2"] = {
+        type = "DEMO",
+        expiry = "2026-09-20",
+        valid = true,
+        max_devices = 14,
         SLOT = "2"
     },
     ["SINGLE_USER"] = {
